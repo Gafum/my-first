7c3b1cd7211cee0692500b87f52d56b933b05ecf
Dima Matrchuk <di221244a@gmail.com>
2022-06-30 19:55:32 +0300

    first commit, add all files

---
+++ script.js
@@ -0,0 +1,7 @@
+function active() {
+  document.querySelector(".contacts").classList.toggle("active");
+  document.querySelector(".menu-inner").classList.toggle("active-btn");
+  document.querySelector("body").classList.toggle("lock");
+}
+
+
